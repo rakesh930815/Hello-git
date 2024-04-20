a3207b72ccbd3bcc940c4898bd96f4c6cbcf0800
rakesh930813 <rakesh.cbspl@gmail.com>
2024-04-20 20:23:47 +0530

    my first test

---
+++ x.py
@@ -0,0 +1,11 @@
+git config --gloabal user.name
+git config --list
+pwd - find parent dir
+mkdir - create dir
+cd x/ - move to x dir
+cd.. - git one step back dir
+ls - list file inside dir
+cat x - get insider content of x
+touch x.py - create file x.py
+
+
